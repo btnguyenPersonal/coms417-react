bd6ff660c812fd711cfb0529fbf8c62f09e8af8b
mmila38 <mmila33001@gmail.com>
2022-04-29 17:37:41 -0500

    added two tests

--- simple-login/src/Login/Login.test.js
+++ simple-login/src/Login/Login.test.js
@@ -15,3 +15,29 @@ test('pressing submit with no info', async () => {
     await fireEvent.click(form);
     expect(screen.queryByText("User is successfully logged in")).toBeNull();
 });
+
+test('successful user1 login', async () => {
+  render(<Login />);
+  const form = screen.getByRole("login");
+  const username = 'user1';
+  const pass = 'football';
+  const userNameInput = screen.getByText('Username');
+  const passwordInput = screen.getByText('Password');
+  userEvent.type(userNameInput, username);
+  userEvent.type(passwordInput, pass);
+  await fireEvent.click(form);
+  expect(screen.queryByText("User is successfully logged in"));
+});
+
+test('failed user1 login', async () => {
+  render(<Login />);
+  const form = screen.getByRole("login");
+  const username = 'user1';
+  const pass = 'soccer';
+  const userNameInput = screen.getByText('Username');
+  const passwordInput = screen.getByText('Password');
+  userEvent.type(userNameInput, username);
+  userEvent.type(passwordInput, pass);
+  await fireEvent.click(form);
+  expect(screen.queryByText("invalid password"));
+});
