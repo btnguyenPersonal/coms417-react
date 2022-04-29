34cffd3633e4d14216ec20c42dbfcd98e50b9dea
btnguyenPersonal <btnguyenPersonal@github.com>
2022-04-28 21:17:59 -0500

    added test for empty input fields

--- simple-login/src/Login/Login.test.js
+++ simple-login/src/Login/Login.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { getByRole, fireEvent } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 
 test('renders title', () => {
-  render(<Login />);
-  const titleElement = screen.getByText("Sign In");
-  expect(titleElement).toBeInTheDocument();
+    render(<Login />);
+    const titleElement = screen.getByText("Sign In");
+    expect(titleElement).toBeInTheDocument();
+});
+
+test('pressing submit with no info', async () => {
+    render(<Login />);
+    const form = screen.getByRole("login");
+    await fireEvent.click(form);
+    expect(screen.queryByText("User is successfully logged in")).toBeNull();
 });
