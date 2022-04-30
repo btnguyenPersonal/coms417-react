96177d84bad8d5bf5a0c5d2ab1c098d12e9dbbfb
btnguyenPersonal <btnguyenPersonal@github.com>
2022-04-30 13:10:35 -0500

    adding correct login test

--- simple-login/src/Login/Login.test.js
+++ simple-login/src/Login/Login.test.js
@@ -15,3 +15,14 @@ test('pressing submit with no info', async () => {
     await fireEvent.click(form);
     expect(screen.queryByText("User is successfully logged in")).toBeNull();
 });
+
+test('pressing submit with correct info', async () => {
+    render(<Login />);
+    const form = screen.getByRole("login");
+    const usernameField = screen.getByTestId("usernameField");
+    await userEvent.type(usernameField, 'user1');
+    const passwordField = screen.getByTestId("passwordField");
+    await userEvent.type(usernameField, 'football');
+    await fireEvent.click(form);
+    expect(screen.queryByText("User is successfully logged in"));
+});
